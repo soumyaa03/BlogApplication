a2b98e24740037300eb76fef9aacb81be8e07de9
soumyaa03 <82571869+soumyaa03@users.noreply.github.com>
2023-12-04 21:58:53 +0530

    changes

--- blogging website - frontend/src/components/navbar.component.jsx
+++ blogging website - frontend/src/components/navbar.component.jsx
@@ -1,12 +1,12 @@
 import logo from "../imgs/logo.png";
- import {Link} from "react-router-dom"
  import React from 'react';
+ import { Link } from "react-router-dom"
 const  Navbar = () => {
     return (
         <nav className="navbar">
-            <Link to="/" className="flex-none w-10">
+            <Link></Link>
             <img src={logo} className="w-full"/>
-            </Link>
+            
         </nav>
     )
 }

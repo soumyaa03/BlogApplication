77d89906f3ec7e75a33e3fbad6c33ff55a2f9f5d
soumyaa03 <82571869+soumyaa03@users.noreply.github.com>
2023-12-10 04:03:42 +0530

    connected userAuthForm to backend

--- blogging website - frontend/src/pages/userAuthForm.page.jsx
+++ blogging website - frontend/src/pages/userAuthForm.page.jsx
@@ -1,14 +1,72 @@
-import React from "react"
+import React, { useRef } from "react"
 import InputBox from "../components/input.component";
 import googleIcon from "../imgs/google.png"
 import { Link } from "react-router-dom";
-
-
+import { Toaster,toast} from "react-hot-toast";
+import axios from "axios";
 const UserAuthForm = ({type}) => {
+
+    let emailRegex = /^\w+([.-]?\w+)*@([.-]?\w+)*(\.\w{2,3})+$/;
+    let passwordRegex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,20}$/;
+
+    const authForm = useRef();
+
+    const register = (serverRoute, formData) => {
+
+        axios.post(import.meta.env.VITE_SERVER_DOMAIN + serverRoute, formData)
+        .then(({data}) => {
+            console.log(data)
+        })
+        .catch(({response}) => {
+            toast.error(response.data.error)
+        })
+    }
+
+    const handleSubmit = (e) => {
+
+        e.preventDefault();
+
+        let serverRoute = type === "sign-in" ? "/login" : "/register"
+
+        //form data
+        let form = new FormData(authForm.current)
+        let formData = {}
+
+        for(let [key,value] of form.entries()){
+            formData[key] = value;
+
+        }
+        
+        let {fullname , email, password} = formData;
+
+        //form validation
+
+        if(fullname){
+            if(fullname.length < 3){
+                return toast.error("Fullname must be at least 3 character long")
+            }
+        }
+
+        if(!email.length){
+            return toast.error("Enter email")
+        }
+
+        if(!emailRegex.test(email)){
+            return toast.error("Email is invalid")
+        }
+
+        if(!passwordRegex.test(password)){
+            return toast.error("Password should be 6 to 20 characters long with a numeric, 1 lowercase and 1 uppercase letter")
+        }
+
+        register(serverRoute,formData)
+
+    }
 
     return (
         <section className="h-cover flex items-center justify-center">
-            <form className="w-[80%] max-w-[400px]">
+            <Toaster/>
+            <form ref={authForm} className="w-[80%] max-w-[400px]">
                 <h1 className="text-4xl font-gelasio capitalize text-center mb-16">
                     {type === "sign-in" ? "Welcome back" : "Join us today"}
                 </h1>
@@ -40,7 +98,10 @@ const UserAuthForm = ({type}) => {
                         icon="fi-rr-key"
                     />
 
-                    <button className="btn-dark center mt-14" type="submit">
+                    <button className="btn-dark center mt-14"
+                     type="submit"
+                     onClick={handleSubmit}
+                     >
                         {type.replace("-"," ")}
                     </button>
 
